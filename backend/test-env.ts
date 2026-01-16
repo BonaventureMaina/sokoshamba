500c840c5ee34d6262668f278ece1ce98563e83e
BonaventureMaina <bonaventuremaina946@gmail.com>
2026-01-16 09:14:12 +0300

    Clean

--- backend/test-env.ts
+++
@@ -1,8 +0,0 @@
-console.log('Script started');
-import dotenv from 'dotenv';
-console.log('Dotenv imported');
-
-const result = dotenv.config();
-console.log('Config result:', result);
-console.log('JWT_SECRET exists:', !!process.env.JWT_SECRET);
-console.log('JWT_REFRESH_SECRET exists:', !!process.env.JWT_REFRESH_SECRET);
